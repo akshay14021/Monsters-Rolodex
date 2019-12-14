c743daa6c75429138c64854d979d671ee203b334
Akshay Joshi <32253592+akshay14021@users.noreply.github.com>
2019-12-14 12:59:39 -0500

    Updates

---
+++ precache-manifest.8b7334c153fdd5880f59b9fbb925dcf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483f50f9ff615ca8144fc8bb049c4aa5",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "dd4ecb51c0e8dbbab785",
+    "url": "/Monsters-Rolodex/static/css/main.427ca7fd.chunk.css"
+  },
+  {
+    "revision": "105422028b6763bd0844",
+    "url": "/Monsters-Rolodex/static/js/2.f35d3ce1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.f35d3ce1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd4ecb51c0e8dbbab785",
+    "url": "/Monsters-Rolodex/static/js/main.72220746.chunk.js"
+  },
+  {
+    "revision": "29f656592ba48eaf223e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.166da6c4.js"
+  }
+]);
