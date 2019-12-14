de8da0884d484d05fe8666adfcbe20369b56e1d0
Akshay Joshi <32253592+akshay14021@users.noreply.github.com>
2019-12-14 13:00:43 -0500

    github pages integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-list/card-list'
+import { SearchBox } from './components/search-box/search-box'
+
+class App extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			monsters: [],
+			searchField: ''
+		}
+	}
+
+	async componentDidMount() {
+		const response = await fetch(
+			'https://jsonplaceholder.typicode.com/users'
+		)
+		const data = await response.json()
+		this.setState({
+			monsters: data
+		})
+	}
+
+	handleInputField = e => {
+		this.setState({
+			searchField: e.target.value
+		})
+	}
+
+	render() {
+		const filteredMonster = this.state.monsters.filter(monster => {
+			return monster.name
+				.toLowerCase()
+				.includes(this.state.searchField.toLowerCase())
+		})
+		return (
+			<div className='App'>
+				<h1>{this.props.title}</h1>
+				<SearchBox handleInputField={this.handleInputField} />
+				<CardList monsters={filteredMonster} />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/search-box/search-box.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './search-box.css'
+
+export const SearchBox = props => {
+	return (
+		<input
+			className='search'
+			type='search'
+			placeholder='search monsters'
+			onChange={props.handleInputField}
+		/>
+	)
+}
